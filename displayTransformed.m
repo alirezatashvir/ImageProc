5c0291c456ebaa51dfc6c6a0a9eb1a1eb14ec728
Alireza Tashvir <alireza.tashvir@gmail.com>
2015-10-08 16:14:18 +0200

    added files

--- displayTransformed.m
+++ displayTransformed.m
@@ -1,7 +1,7 @@
 function tfImage = displayTransformed(image)
 
     absF = fftshift(log(1+abs(fft2(image))));
-    figure; imshow(image, []);
+    %figure; imshow(image, []);
     figure; imshow(absF, []);
     tfImage = absF;
 end
